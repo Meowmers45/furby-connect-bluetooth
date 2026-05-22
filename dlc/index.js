35d68ff4065d00c332548b05dc65a178c88d79f4
Aaron Johnson <dynomitekid101+1@gmail.com>
2026-05-22 07:27:13 -0500

    furby connect

---
+++ dlc/index.js
@@ -0,0 +1,95 @@
+[
+    { 
+        "file": "tu011880.dlc", 
+        "title": "Context Logo",
+        "buttons": [
+            { "title": "Logo", "action": [75,0,4,4] }
+        ]
+    },
+    { 
+        "file": "tu013530.dlc", 
+        "title": "HACKED",
+        "buttons": [
+            { "title": "Hacked 1", "action": [75,0,3,4] },
+            { "title": "Hacked 2", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "noimg2.dlc", 
+        "title": "Audio only",
+        "buttons": [
+            { "title": "Audio test ", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu00dk.dlc", 
+        "title": "DKRap DLC",
+        "buttons": [
+            { "title": "DK Rap ", "action": [75,0,0,0] }
+        ]
+    },
+    {
+        "file": "tu00rick.dlc", 
+        "title": "Rick Roll",
+        "buttons": [
+            { "title": "Rick Roll ", "action": [75,0,0,0] }
+        ]
+    },
+    {
+        "file": "chilli.dlc", 
+        "title": "Chilli",
+        "buttons": [
+            { "title": "Chilli ", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu011880.dlc", 
+        "title": "Offical1",
+        "buttons": [
+            { "title": "Furb 1 ", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu013530.dlc", 
+        "title": "Offical2",
+        "buttons": [
+            { "title": "Furb 2 ", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu012700.dlc", 
+        "title": "Offical3",
+        "buttons": [
+            { "title": "Furb 3 ", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu10tcfi.dlc", 
+        "title": "Freddy",
+        "buttons": [
+            { "title": "Uh oh", "action": [75,0,0,0] },
+            { "title": "Uh oh again", "action": [75,0,0,1] }
+        ]
+    },
+    {
+        "file": "tu00crep.dlc", 
+        "title": "Spooky",
+        "buttons": [
+            { "title": "Spoopy", "action": [75,0,0,0] }
+        ]
+    },
+        {
+        "file": "tu00ball.dlc", 
+        "title": "Ballin",
+        "buttons": [
+            { "title": "Ballin", "action": [75,0,0,0] }
+        ]
+    },
+    {
+        "file": "tu00mond.dlc", 
+        "title": "Monday",
+        "buttons": [
+            { "title": "Monday", "action": [75,0,0,0] }
+        ]
+    }
+]
